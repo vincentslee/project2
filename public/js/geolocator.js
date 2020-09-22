de1758f4331b8003ea788b38e8350f3d05e39a6e
Vincent Lee <911vincentlee@gmail.com>
2020-09-22 18:58:36 -0500

    Edited syntax

--- public/js/geolocator.js
+++ public/js/geolocator.js
@@ -3,15 +3,18 @@ var options = {
     timeout: 5000,
     maximumAge: 0
   };
-  
+
+//Function upon succes
 function success(pos) {
     return pos;
 
 }
 
+//Function upon failure
 function error(err) {
 console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
+//Prompts geolocator
 module.exports = navigator.geolocation.getCurrentPosition(success, error, options);
 

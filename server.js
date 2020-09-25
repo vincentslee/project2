0fded5c27124335336aa3ee78ddf110f5a6b16f5
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-09-25 17:09:02 -0500

    handlebars update

--- server.js
+++ server.js
@@ -20,9 +20,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our routes
-require("./routes/html-routes.js")(app);
+
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {

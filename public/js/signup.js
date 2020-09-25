1a258195c9a614e30c0e3083305150470101532e
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-09-25 18:35:31 -0500

    fixing bugs with signin

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ $(document).ready(() => {
       password: password
     })
       .then(() => {
-        res.render("index");
+        window.location.replace("/members");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);

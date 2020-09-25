7ad084c32fbbec6d8ed254e3614036e38d5b7837
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-09-24 19:01:08 -0500

    update to tables, models, JS, members.html

--- models/user.js
+++ models/user.js
@@ -16,6 +16,18 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    happiness: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    position: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
